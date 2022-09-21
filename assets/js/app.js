129a5d364dedef0f8449c86c89891797b74e2f25
Jefferson Pulido <86859723+JeffersonPulido@users.noreply.github.com>
2022-09-21 15:39:14 -0500

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,75 @@
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('.meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+
+//lsiteners
+
+searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', getMealRecipe);
+recipeCloseBtn.addEventListener('click', () => {
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
+});
+
+//functions
+function getMealList(){
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    .then(response => response.json())
+    .then(data => {
+        let html = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                html += `
+                    <div class="meal-item" data-id="${meal.idMeal}">
+                        <div class="meal-img">
+                            <img src="${meal.strMealThumb}" alt="food" />
+                        </div>
+                        <div class="meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href="#" class="recipe-btn">Ver Receta</a>
+                        </div>
+                    </div>
+                `;
+            });
+            mealList.classList.remove('notFound');
+        }else{
+            html = `Lo siento, no hemos encontrado ninguna receta con ${searchInputTxt}`
+            mealList.classList.add('notFound');
+        }
+
+
+        mealList.innerHTML = html;
+    })
+}
+
+function getMealRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal(data.meals[0]));
+    }
+}
+
+function mealRecipeModal(meal){
+    let htmlModalRecipes = "";
+    htmlModalRecipes += `
+        <h2 class="recipe-title">${meal.strMeal}</h2>
+        <p class="recipe-category">${meal.strCategory}</p>
+        <div class="recipe-instruct">
+            <h3>Instrucciones: </h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class="recipe-meal-img">
+            <img src="${meal.strMealThumb}" alt="">
+        </div>
+        <div class="recipe-link">
+            <a href="${meal.strYoutube}" target="_blank">Ver Video</a>
+        </div>
+    `;
+    mealDetailsContent.innerHTML = htmlModalRecipes;
+    mealDetailsContent.parentElement.classList.add('showRecipe');
+}
+
